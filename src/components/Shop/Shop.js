c775c0c4c3e72e5568974821846ee53cd2db0524
sabbirpboss <sabbir.ah799@gmail.com>
2022-03-26 00:41:13 +0600

    products.json fakedata created

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,9 +3,14 @@ import './Shop.css'
 
 const Shop = () => {
     return (
-        <div>
-            <h1>this is shop.js</h1>
-        </div>
+        <section className='shop-container'>
+            <div className="products-container">
+                <h2>this is products.</h2>
+            </div>
+            <div className="selection-container">
+                <h2>this is selector</h2>
+            </div>
+        </section>
     );
 };
 
