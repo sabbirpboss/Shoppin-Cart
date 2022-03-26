b82678ecef32c380eead6597fbadc2a3eed9072f
sabbirpboss <sabbir.ah799@gmail.com>
2022-03-26 20:45:22 +0600

    random uses done and double added prolem solved

--- src/components/Cart/Selected.js
+++ src/components/Cart/Selected.js
@@ -4,10 +4,10 @@ import './Selected.css'
 const Cart = () => {
     return (
         <div>
-            <h1 className='selected-items'>Selected Items</h1>
-            <div>
+            {/* <h1 className='selected-items'>Selected Items</h1> */}
+            {/* <div>
                 <p>show item here</p>
-            </div>
+            </div> */}
             <button className='choose-btn'>Choose 1 For Me</button>
             <br />
             <button className='choose-again-btn'>Choose Again</button>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -9,8 +9,14 @@ const Shop = () => {
     // console.log(cart);
 
     const handleAddToCart = (singleProduct) => {
-        const newCart = [...cart, singleProduct];
-        setCart(newCart);
+        const addedProduct = cart.find((item) => item.id === singleProduct.id);
+        if(addedProduct){
+            alert("Already Added");
+        }
+        else{
+            const newCart = [...cart, singleProduct];
+            setCart(newCart);
+        }
     }
 
     useEffect( () => {
@@ -18,6 +24,16 @@ const Shop = () => {
         .then(res => res.json())
         .then(data => setProducts(data))
     }, [])
+
+    const randomOne = () => {
+        const random = cart[Math.floor(Math.random() * cart.length)];
+        const newCarts = [random];
+        setCart(newCarts);
+    }
+    const resetButton = () => {
+        const reset = [];
+        setCart(reset);
+    }
     return (
         <section className='shop-container'>
             <div className="products-container">
@@ -28,9 +44,13 @@ const Shop = () => {
                 }
             </div>
             <div className="selection-container">
+                <h1 className='selected-items'>Selected Items:</h1>
                 {
-                    cart.map(item => <h2 key = {item.id}>{item.name}</h2>)
+                    cart.map(item => <h2 className='custom-color' key = {item.id}>{item.name}</h2>)
                 }
+                <button onClick={randomOne}>Random Choose</button>
+                <button onClick={resetButton}>Clear all</button>
+                {/* <Selected></Selected> */}
             </div>
         </section>
     );
