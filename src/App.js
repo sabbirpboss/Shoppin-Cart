86b28fa3635b8024772b40418862300756c8b6dc
sabbirpboss <sabbir.ah799@gmail.com>
2022-03-26 23:00:51 +0600

    some style added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
+import Question from './components/Question/Question';
 import Shop from './components/Shop/Shop';
 
 function App() {
   return (
-    <div>
+    <div className='for-width'>
       <div className="App">
         <h1 className='custom-color-green'>Hey, Confused? Get Ready.
         </h1>
         <h2 className='custom-color-blueviolet'>Choose Your Favourite but Only Four!!!</h2>
       </div>
       <Shop></Shop>
+      <Question></Question>
     </div>
   );
 }
---
+++ src/components/Question/Question.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Question.css'
+
+const Question = () => {
+    return (
+        <div className='question'>
+            <div>
+                <h3>Question:1: How React works?</h3>
+                <p>Answer: </p>
+            </div>
+            <div>
+                <h3>Question:2: props vs state?</h3>
+                <p>Answer: </p>
+            </div>
+        </div>
+    );
+};
+
+export default Question;
